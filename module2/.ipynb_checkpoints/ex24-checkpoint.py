e096c42df7be850580c44b871314518124554f70
vieira-klgwn <ntwavieir7@gmail.com>
2025-07-22 16:55:13 +0200

    finalized ex24

--- module2/.ipynb_checkpoints/ex24-checkpoint.py
+++ module2/.ipynb_checkpoints/ex24-checkpoint.py
@@ -12,10 +12,16 @@ cars = [
     {'type':'BMW', 'color':'White', 'size':'Baby','miles': 7890}
 ]
 
-languages = 
+languages = [
     {'name':'Python', 'speed':'Slow', 'opinion':['Terrible', 'Mush']},
     {'name':'JavaScript', 'speed':'Moderate','opinion':['Alright', 'Bizarre']},
     {'name':'C++', 'speed':'Fast', 'opinion':['Annoying', 'Dangerous']},
     {'name':'Forth', 'speed':'Fast','opinion': ['Fun','Difficult']},
     {'name':'Perl6','speed':'Moderate','opinion': ['Fun', 'Weird']}
 ]
+
+print(f"In fruits, 12 is {fruit[0]['count']} and 'AAA' is {fruit[0]['rating']} and 14 is {fruit[3]['count']} and Apples is {fruit[0]['kind']}")
+
+print(f"In cars, Big is {cars[0]['size']} and White is {cars[3]['color']}")
+
+print(f"In languages, Slow is {languages[0]['speed']} and Alright is {languages[1]['opinion'][0]} and Moderate is {languages[4]['speed']}")
