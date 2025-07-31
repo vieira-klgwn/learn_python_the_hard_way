533e48046c5be192202092fe6650cf05e5fba948
vieira-klgwn <ntwavieir7@gmail.com>
2025-07-31 22:05:30 +0200

    finalized ex52 and did a webscraper

---
+++ module3/.ipynb_checkpoints/web_scrapper-checkpoint.py
@@ -0,0 +1,60 @@
+from bs4 import BeautifulSoup
+from urllib import request
+from urllib.parse import urljoin
+import os
+import re
+import pprint 
+import pdftotext
+
+
+# make a directory to put in the pdfs from  the link 'https://learncodethehardway.com/setup/python/ttb/'
+DOWNLOAD_DIR = 'ttb_pdfs'
+DIRECTORY = os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+
+# request and download the website
+resp = request.urlopen('https://learncodethehardway.com/setup/python/ttb/')
+content = resp.read()
+
+# access all links to the pdfs and download them download the pdfs 
+pdf_links =[]
+pdf_num = 1
+BASE_URL = 'https://learncodethehardway.com/setup/python/ttb/'
+soup = BeautifulSoup(content,'html5lib' )
+for link in soup.find_all('a', href=True):
+    href = link['href']
+    if href.lower().endswith('.pdf') and pdf_num <= 5:
+        full_url = urljoin(BASE_URL,href)
+        pdf_links.append(full_url)
+        pdf_num += 1
+    else:
+        pass
+
+
+
+# download the files from the link
+
+
+for pdf_link in pdf_links:
+    filename = os.path.basename(pdf_link)
+    filepath = os.path.join(DOWNLOAD_DIR, 'ttb_pdfs')
+    if not os.path.exists(filepath):
+        print(f"📥 Downloading {filename}...")
+        try:
+            file = request.urlopen(pdf_link)
+            content = pdftotext.PDF(file)
+            # processs file content to list of lines
+            lines = "".join(content).split("\n")
+            for line in lines:
+                # do data munging on them
+                print(line)
+
+        except Exception as e:
+            print(f"❌ Failed to download {pdf_link}: {e}")
+    else:
+        print(f"✅ Already downloaded: {filename}")
+        
+            
+    
+    
+    
+
--- module3/ex52.py
+++ module3/ex52.py
@@ -3,50 +3,66 @@ from urllib import request
 import os
 import re
 
-pages = 0
-page_list = []
-if not os.path.exists('ttb_page.html'):
-    while pages < 5:
-        with open(f'ttb_page{pages}.html','wb') as f:
-        resp = request.urlopen('https://learncodethehardway.com/setup/python/ttb/')
-        body = resp.read() # make each body be appended in a list.
-        f.write(body)
-        page_list.append(body) # appending to the list, I don't know if this is correct
-        pages += 1
+# pages = 0
+# page_list = []
+# if not os.path.exists('ttb_page0.html'):
+#     while pages < 5:
+#         with open(f'ttb_page{pages}.html','wb') as f:
+#         resp = request.urlopen('https://learncodethehardway.com/setup/python/ttb/')
+#         body = resp.read() # make each body be appended in a list.
+#         f.write(body)
+#         page_list.append(body) # appending to the list, I don't know if this is correct
+#         pages += 1
     
-else:
-    read_pages = 0
-    while read_pages < 5:
-        with open(f'ttb_page{read_pages}.html', encoding='utf-8') as f:
-        body = f.read()
-        page_list.append(body) # appending to the list, I don't know if this is correct
-        read_pages += 1
+# else:
+#     read_pages = 0
+#     while read_pages < 5:
+#         with open(f'ttb_page{read_pages}.html', encoding='utf-8') as f:
+#         body = f.read()
+#         page_list.append(body) # appending to the list, I don't know if this is correct
+#         read_pages += 1
         
-    
+
+if not os.path.exists('ttb_page.html'):
+    with open('ttb_page.html','wb') as f:
+        resp = request.urlopen('https://learncodethehardway.com/setup/python/ttb/')
+        body = resp.read()
+        f.write(body)
+else:
+    with open('ttb_page.html', 'rb') as f:
+        body = f.read()
+
+soup = BeautifulSoup(body,'html5lib')
+print('Soup :' , soup)
+print("Title:", soup.title)
+print("\n Body:" , soup.body)
+
+
+
 
 # now let us parse the html (body) we have into a text so we can interact with it
 # if the code above works, then the aproach here can be using a loop that loops in each of the five files, and another one that loops in each line of the file. Remember that to loop in each line of the file requires also joining and stripping as in ex51
 
-soup = BeautifulSoup(body,'html5lib')
+# soup = BeautifulSoup(body,'html5lib')
 
-print("The whole object is here: ",soup)
-print("This is the title",soup.title)
+# print("The whole object is here: ",soup)
+# print("This is the title",soup.title)
 
-# let us play with the data that we have ,using regex, 
+# # let us play with the data that we have ,using regex, 
 
-content_body = soup.body
+# content_body = soup.body
 
-lines = "".join(content_body).strip('\n')
-# now let us use regex
+# lines = "".join(content_body).strip('\n')
+# # now let us use regex
 
-num = 1
-for line in lines:
-    search = re.search('(Report Period)', line)
-    if search:
-        print(f"[{num}]: {search.group()}")
-        num += 1
-    else:
-        pass
+# num = 1
+# for line in lines:
+#     search = re.search('(Report Period)', line)
+#     if search:
+#         print(f"[{num}]: {search.group()}")
+#         num += 1
+#     else:
+#         pass
 
 
         
