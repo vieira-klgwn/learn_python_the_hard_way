5aad747e68fbba5a5b76ff73b9afdc7fd34a558d
vieira-klgwn <ntwavieir7@gmail.com>
2025-08-01 13:52:07 +0200

    Added a jupyter notebook with pandas 10min guide practice

--- module3/ex53.py
+++ module3/ex53.py
@@ -2,6 +2,7 @@ import requests
 from pprint import pprint
 import sys
 import csv
+import sys
 
 api_url = "http://learncodethehardway.com/api/course"
 # get all courses
@@ -14,6 +15,26 @@ r = requests.get(api_url, params={
 })
 
 data = r.json()
-pprint(data)
+if isinstance(data, dict):
+    #wrap it up in a list
+    data = [data]
+else:
+    exit(1)
 
+# import data to csv
+with open('downloaded.csv','w', newline='', encoding='utf-8') as f:
+    # getting field names
+    fieldnames = data[0].keys()
+    print("The keys are {}".format(fieldnames))
+
+    # getting the writer
+    writer = csv.DictWriter(f, fieldnames= fieldnames)
+
+    # now write the column names or let's call them headers for now
+    writer.writeheader()
+
+    # now write the row names
+    writer.writerows(data)
+    
+    
 
