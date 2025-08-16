cbeca151995c9b8dccb969a4ea5d14bdb6f74732
vieira-klgwn <ntwavieir7@gmail.com>
2025-08-16 21:42:55 +0200

    added some automated tests

--- django/first_django/main/views.py
+++ django/first_django/main/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render,get_object_or_404
 from django.db.models import F
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 
 
@@ -15,18 +16,28 @@ class IndexView(generic.ListView):
 	context_object_name = 'latest_question_list'
 	def get_queryset(self):
 		"""Return the last five published questions."""
-		return Question.objects.order_by('-pub_date')[:5]
+		# return Question.objects.order_by('-pub_date')[:5]
+		"""Return the last five published questions(not incluceing those 
+		set to be published in the future)"""
+
+		return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
+
 
 
 
 class DetailView(generic.DetailView):
-	model = Question
 	template_name = 'polls/detail.html'
+	def get_queryset(self):
+		"""Excludes any questions that aren't published yet.'"""
+		return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultView(generic.DetailView):
-	model = Question
+	# model = Question
 	template_name = 'polls/result.html'
+	def get_queryset(self):
+		"""Excludes any questions that aren't published yet."""
+		return Question.objects.filter(pub_date__lte=timezone.now())
 def vote(request, question_id):
 	question = get_object_or_404(Question, pk=question_id)
 	try:
