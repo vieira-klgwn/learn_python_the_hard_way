0fd02115a2eee51cdce74fb1dc0ce3ecb16f236e
vieira-klgwn <ntwavieir7@gmail.com>
2025-08-18 23:45:11 +0200

    enhanced the admin panel

--- django/first_django/main/admin.py
+++ django/first_django/main/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 
 # Register your models here
 
-from .models import Question
+from .models import Question, Choice
 
-admin.site.register(Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields" : [ 'question_text']}),
+        ("Date Information", {"fields":['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ['pub_date', 'question_text', 'was_published_recently']
+    search_fields = ['question_text']
+
+
+admin.site.register(Question, QuestionAdmin)
